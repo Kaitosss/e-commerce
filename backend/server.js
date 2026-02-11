0fb249801a2d7f907e6c6004f75f74a40685a640
Boom <nuruto123456789za@gmail.com>
2026-02-11 18:24:32 +0700

    add database configuration

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -9,4 +10,5 @@ app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port : " + PORT);
+  connectDB();
 });
