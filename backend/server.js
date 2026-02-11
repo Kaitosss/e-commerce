281b1f96efa4dba3972a2393471c8dcb27c075ae
Boom <nuruto123456789za@gmail.com>
2026-02-11 17:55:24 +0700

    add auth router and controller

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import "dotenv/config";
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  console.log("Server is running on port : " + PORT);
+});
