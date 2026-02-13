06490e3b9045ab1a6cf7ceca6b4bac3f49c44139
Boom <nuruto123456789za@gmail.com>
2026-02-13 18:56:07 +0700

    add express.json, update user model and enhance signup controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,25 @@
-export const signup = (req, res) => {};
+import User from "../models/user.model.js";
 
-export const login = (req, res) => {};
+export const signup = async (req, res) => {
+  const { email, password, name } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({ name, email, password });
+
+    res.status(201).json({ user, message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const login = (req, res) => {
+  try {
+  } catch (error) {}
+};
 
 export const logout = (req, res) => {};
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./config/db.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
