5831f81f93f5f4b3cb66366d8fa2a295086db35d
Boom <nuruto123456789za@gmail.com>
2026-02-14 12:19:15 +0700

    add product routes, controller and model

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -37,3 +37,11 @@ export const protectRoute = async (req, res, next) => {
       .json({ message: "Unauthorized - Invalid access token" });
   }
 };
+
+export const adminRoute = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied - Admin only" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 5000;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log("Server is running on port : " + PORT);
