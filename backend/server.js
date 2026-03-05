2d689e2daa0dc4e9f1ff7cca1561da5dc8feaa30
boomdev <nuruto123456789za@gmail.com>
2026-03-05 08:41:33 +0700

    Project completed

--- backend/server.js
+++ backend/server.js
@@ -8,9 +8,11 @@ import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 import { connectDB } from "./config/db.js";
+import path from "path";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -21,6 +23,16 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "..", "frontend", "dist", "index.html"),
+    );
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on port : " + PORT);
